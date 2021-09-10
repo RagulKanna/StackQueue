e41372c78329c8ebbb414c9f2bd6d411f7472e23
RagulKanna <ragulragi619@gmail.com>
2021-09-10 16:24:55 +0530

    Added:Add elements in stack

---
+++ src/StackQueue/StackQueueMain.java
@@ -0,0 +1,21 @@
+package StackQueue;
+
+import java.util.Scanner;
+
+public class StackQueueMain {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.print("Enter \n 1 to add element at the start  " +
+                "\n choice: ");
+
+        switch (sc.nextInt()) {
+            case 1:
+                Operation.addelement();
+                break;
+
+
+        }
+
+    }
+}
